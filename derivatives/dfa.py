4af6695dd700ba52ab89410890fe108ff6c5cc6a
ethframe <ethframe@gmail.com>
2020-12-05 14:24:09 +0300

    Add crude c lexer generation

--- derivatives/dfa.py
+++ derivatives/dfa.py
@@ -1,7 +1,9 @@
+from contextlib import contextmanager
 import html
 from collections import defaultdict, deque
+from io import StringIO
 from itertools import count
-from typing import Callable, Dict, Iterator, List, Optional, Set, Tuple
+from typing import Any, Callable, Dict, Iterator, List, Optional, Set, Tuple
 
 from .core import Regex
 
@@ -68,59 +70,143 @@ class Dfa:
                 return "\\" + char
             return html.escape(char).encode("unicode_escape").decode("ascii")
 
-        buf = [
-            "digraph dfa {",
-            "  rankdir=LR",
-            '  "" [shape=none]',
-            '  "" -> "0"'
-        ]
+        buf = Buffer(2)
+        buf.line("digraph dfa {")
+        with buf.indent():
+            buf.line("rankdir=LR")
+            buf.line('"" [shape=none]')
+            buf.line('"" -> "0"')
 
-        seen_tags: Set[str] = set()
-        for state, tag in enumerate(self._tags):
-            shape = "circle"
-            if tag is not None:
-                shape = "doublecircle"
-                seen_tags.add(tag)
-            buf.append(
-                '  "{}" [shape={} fixedsize=shape]'.format(state, shape)
-            )
+            seen_tags: Set[str] = set()
+            for state, tag in enumerate(self._tags):
+                shape = "circle"
+                if tag is not None:
+                    shape = "doublecircle"
+                    seen_tags.add(tag)
+                buf.format('"{}" [shape={} fixedsize=shape]', state, shape)
 
-        for tag in sorted(seen_tags):
-            buf.append(
-                '  "t_{0}" [shape=rect style=dashed label="{0}"]'.format(tag)
-            )
+            for tag in sorted(seen_tags):
+                buf.format(
+                    '"t_{0}" [shape=rect style=dashed label="{0}"]', tag
+                )
 
-        for state, trantisions in enumerate(self._delta):
-            compact: Dict[int, List[Tuple[str, str]]] = defaultdict(list)
-            for start, end, target in trantisions:
-                compact[target].append((start, end))
-            for target, ranges in compact.items():
-                classes = []
-                for start, end in ranges:
-                    size = ord(end) - ord(start) + 1
-                    if size == 1:
-                        classes.append(fmt_char(start))
-                    elif size <= 3:
-                        classes.append(
-                            "".join(
-                                fmt_char(chr(c))
-                                for c in range(ord(start), ord(end) + 1)
+            for state, trantisions in enumerate(self._delta):
+                compact: Dict[int, List[Tuple[str, str]]] = defaultdict(list)
+                for start, end, target in trantisions:
+                    compact[target].append((start, end))
+                for target, ranges in compact.items():
+                    classes = []
+                    for start, end in ranges:
+                        size = ord(end) - ord(start) + 1
+                        if size == 1:
+                            classes.append(fmt_char(start))
+                        elif size <= 3:
+                            classes.append(
+                                "".join(
+                                    fmt_char(chr(c))
+                                    for c in range(ord(start), ord(end) + 1)
+                                )
                             )
-                        )
-                    else:
-                        classes.append(fmt_char(start) + "-" + fmt_char(end))
-                label = "[{}]".format("".join(classes))
-                buf.append(
-                    '  "{}" -> "{}" [label=<{}>]'.format(state, target, label)
-                )
-            tag = self._tags[state]
-            if tag is not None:
-                buf.append(
-                    '  "{}" -> "t_{}" [style=dashed]'.format(state, tag)
-                )
+                        else:
+                            classes.append(
+                                fmt_char(start) + "-" + fmt_char(end)
+                            )
+                    label = "[{}]".format("".join(classes))
+                    buf.format(
+                        '"{}" -> "{}" [label=<{}>]', state, target, label
+                    )
+                tag = self._tags[state]
+                if tag is not None:
+                    buf.format('"{}" -> "t_{}" [style=dashed]', state, tag)
 
-        buf.extend(["}", ""])
-        return "\n".join(buf)
+        buf.line("}")
+        return buf.getvalue()
+
+    def to_c(self) -> str:
+        buf = Buffer(4)
+        buf.line("#include <stdint.h>")
+        buf.skip()
+        buf.line("#define DFA_NEED_INPUT -1")
+        buf.line("#define DFA_ERROR -2")
+        buf.skip()
+        buf.line("struct Dfa {")
+        with buf.indent():
+            buf.line("unsigned int state;")
+        buf.line("};")
+        buf.skip()
+        buf.line(
+            "static inline void dfa_reset(struct Dfa *dfa) { dfa->state = 0; }"
+        )
+        buf.skip()
+        tokens = sorted({tag for tag in self._tags if tag is not None})
+        for value, name in enumerate(tokens):
+            buf.format("#define T_{} {}", name.upper(), value)
+        buf.skip()
+        buf.line("static const char *dfa_token_name(int t) {")
+        with buf.indent():
+            buf.line("static const char *table[] = {")
+            with buf.indent():
+                for name in tokens:
+                    buf.format('"{}",', name)
+            buf.line("};")
+            buf.format("if (t < 0 || t >= {}) {{ return NULL; }}", len(tokens))
+            buf.format("return table[t];")
+        buf.line("};")
+        buf.skip()
+        buf.line(
+            "static inline int dfa_handle(struct Dfa *dfa, uint32_t c) {"
+        )
+        with buf.indent():
+            buf.line("switch (dfa->state) {")
+            for state, transitions in enumerate(self._delta):
+                buf.format("case {}:", state)
+                with buf.indent():
+                    for start, end, target in transitions:
+                        buf.format(
+                            "if (c < {}) {{ return DFA_ERROR; }}", ord(start)
+                        )
+                        tag = self._tags[target]
+                        if tag is None:
+                            ret = "DFA_NEED_INPUT"
+                        else:
+                            ret = "T_{}".format(tag.upper())
+                        buf.format(
+                            "if (c < {}) {{ dfa->state = {}; return {}; }}",
+                            ord(end) + 1, target, ret
+                        )
+                    buf.line("break;")
+            buf.line("}")
+            buf.line("return DFA_ERROR;")
+        buf.line("}")
+        buf.skip()
+        return buf.getvalue()
+
+
+class Buffer:
+    def __init__(self, indent: int = 2):
+        self._buffer = StringIO()
+        self._indent = " " * indent
+        self._level = 0
+
+    @contextmanager
+    def indent(self) -> Iterator[None]:
+        self._level += 1
+        yield
+        self._level -= 1
+
+    def skip(self, n: int = 1) -> None:
+        self._buffer.write("\n" * n)
+
+    def line(self, s: str) -> None:
+        self._buffer.write(self._indent * self._level)
+        self._buffer.write(s)
+        self._buffer.write("\n")
+
+    def format(self, s: str, *args: Any, **kwargs: Any) -> None:
+        self.line(s.format(*args, **kwargs))
+
+    def getvalue(self) -> str:
+        return self._buffer.getvalue()
 
 
 def make_dfa(regex: Regex, tag_resolver: Callable[[Set[int]], str]) -> Dfa:
