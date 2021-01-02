3c45db01b13dbc0fe043deddf40d95ee2de3b649
ethframe <ethframe@gmail.com>
2021-01-02 16:27:54 +0300

    Remove tag from edsl

--- derivatives/__init__.py
+++ derivatives/__init__.py
@@ -2,13 +2,12 @@ from .codegen import generate_c, generate_dot
 from .dfa import Dfa, make_dfa
 from .edsl import (
     Regex, any_char, any_with, any_without, char, char_range, char_set, empty,
-    epsilon, string, tag
+    epsilon, string
 )
 from .lexer import make_lexer, raise_on_conflict, select_first
 
 __all__ = [
     "Regex", "Dfa", "make_dfa", "any_char", "any_with", "any_without", "char",
-    "char_range", "char_set", "empty", "epsilon", "string", "tag",
-    "make_lexer", "raise_on_conflict", "select_first", "generate_c",
-    "generate_dot"
+    "char_range", "char_set", "empty", "epsilon", "string", "make_lexer",
+    "raise_on_conflict", "select_first", "generate_c", "generate_dot"
 ]
